f2d64f837e23fdb42971f592b782e81221742873
florianempinet <fempinet@gmail.com>
2020-11-04 11:41:37 +0100

    My simple Stripe application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :event
   devise_for :users
-
+  resources :charges
   root "event#index"
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
