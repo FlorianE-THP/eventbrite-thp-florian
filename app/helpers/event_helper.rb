d694766700942401b3743df050e979567edeaf9e
florianempinet <fempinet@gmail.com>
2020-11-04 15:11:51 +0100

    Ca avance

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,2 +1,13 @@
 module EventHelper
+  def can_join?
+    event = Event.find(params[:id])
+
+    if Attendance.where(event_id: current_user.id, event_id: event.id).exists?
+      return false
+    elsif current_user.id == event.organizer_id
+      return false
+    else
+      true
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :charges
   root "event#index"
   resources :users, only: [:show]
+  resources :attendances
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
