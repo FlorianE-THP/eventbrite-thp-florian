a618c9c31885adb27c3c981b0b84b4e95f07ab37
florianempinet <fempinet@gmail.com>
2020-11-06 15:08:56 +0100

    rgjrj

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,42 +1,42 @@
-# class Admin::UsersController < ApplicationController
-#   before_action :check_if_admin
-#   # Methods omitted
-#   def index
-#     @users = User.all
-#   end
+class Admin::UsersController < ApplicationController
+  before_action :check_if_admin
+  # Methods omitted
+  def index
+    @users = User.all
+  end
 
-#   def show
-#     @user = User.find(params[:id])
-#   end
+  def show
+    @user = User.find(params[:id])
+  end
 
-#   def new
-#   end
+  def new
+  end
 
-#   def create
-#   end
+  def create
+  end
 
-#   def edit
-#     @user_edit = User.find(params[:id])
-#     # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
-#   end
+  def edit
+    @user_edit = User.find(params[:id])
+    # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+  end
 
-#   def update
-#     @user = User.find(params[:id])
-#     post_params = params.require(:gossip).permit(:first_name, :last_name, :email, :description, :is_admin)
-#     @user.update(post_params)
-#     redirect_to root_path
-#   end
+  def update
+    @user = User.find(params[:id])
+    post_params = params.require(:gossip).permit(:first_name, :last_name, :email, :description, :is_admin)
+    @user.update(post_params)
+    redirect_to root_path
+  end
 
-#   def destroy
-#     @user = User.find(params[:id])
-#     @user.destroy
-#   end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
 
-#   private
+  private
 
-#   def check_if_admin
-#     unless current_user.admin?
-#       redirect_to root_path
-#     end
-#   end
-# end
+  def check_if_admin
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
+end
